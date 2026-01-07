1df77e735e0eaf80c9212fb024301db696b30610
Viva <happybird00@naver.com>
2026-01-07 16:38:32 +0900

    elk + monitoring

--- src/main/java/org/example/loadtestdemo/domain/service/AdminOrderService.java
+++ src/main/java/org/example/loadtestdemo/domain/service/AdminOrderService.java
@@ -3,6 +3,7 @@ package org.example.loadtestdemo.domain.service;
 import java.time.LocalDateTime;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.example.loadtestdemo.domain.dto.AdminOrderSummaryDto;
 import org.example.loadtestdemo.domain.repository.AdminOrderQueryRepository;
 import org.springframework.stereotype.Service;
@@ -10,12 +11,16 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class AdminOrderService {
 
     private final AdminOrderQueryRepository queryRepository;
 
     @Transactional(readOnly = true)
     public List<AdminOrderSummaryDto> getSummary() {
+        log.info("Info 로그 기록");
+        log.error("Error 로그 기록");
+        log.warn("Warn 로그 기록");
         return queryRepository.findSummary();
     }
 }
